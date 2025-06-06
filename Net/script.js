49bf2b1972a9d9514e5bc0bdfd5162ed10487655
skvortsovvy <skvortsovvy@it.mos.ru>
2025-06-06 21:13:30 +0300

    06_06_25

--- Net/script.js
+++ Net/script.js
@@ -1,5 +1,13 @@
 var $ = ITRP.$;            // jQuery
 var $extension = $(this);  // The UI Extension container with custom HTML
+var vmCounter = 0;
+var maxVM = 10;
+var isShowDell = false;
+var isShowAdd = false;
+var vmName;
+var vmNetworkCidrLabel;
+var inputJson;
+var createVM;
 
 $("#input_form").readonly(true);
 
@@ -13,6 +21,15 @@ function removedClass(selector, className) {
         selector.removeClass(className);
     };
 };
+
+function clearFields(selector, isRequired) {
+    $(selector).val(null);
+    if (isRequired) {
+        $(selector).addClass('required');
+    };
+};
+
+
 
 //если новый запрос из селфсервиса, скрыть поле комментарий
 if (ITRP.record.new) {
@@ -63,26 +80,263 @@ $('#justification').on('change', function () {
 });
 
 
+$('#name_is').on('change', function () {
+    removedClass($('#name_is'), 'empty');
+});
 
-// $("#is_ex_change").on("change", function () {
-//     if ($(this).is(":checked")) {
-//         $("#row_justification").show();
-//     } else {
-//         $("#row_justification").hide();
-//         $("#justification").val(null).change();
-//         $("#inc_desc").parent().hide();
-//         $("#inc_desc").val(null).change();
-//     }
-// });
 
-// $("#justification").on("change", function () {
-//     if ($("#justification").val() === 'для_выполнения_поручения_руковод') {
-//         $("#inc_desc").parent().hide();
-//         $("#inc_desc").val(null).change();
-//     } else {
-//         $("#inc_desc").parent().show();
-//     }
-// });
+var vmN1Operation = $extension.find('#vm_ni_operation');
+vmN1Operation.on('change', function () {
+    isShowAdd = false;
+    isShowDell = false;
+    if (vmN1Operation.val() == 'delete') {
+        isShowDell = true;
+    } else if (vmN1Operation.val() == 'add') {
+        isShowAdd = true;
+    };
+
+    visiabileRemoveNetwork(isShowDell);
+    visiabileAddNetwork(isShowAdd);
+
+});
+
+var vmNetworkcidirAction = $extension.find('#vm_networkcidr_action');
+vmNetworkcidirAction.on('change', function () {
+    var isShow = false;
+    removedClass(vmNetworkcidirAction, "empty");
+    if (vmNetworkcidirAction.val() == 'существующая') {
+        isShow = true;
+    };
+
+    if (!isShow) {
+        clearFields('#vm_networkcidr', false);
+
+    };
+
+    $('#vm_networkcidr_required').toggleClass('required', isShow);
+    toggleRowVisibility('#vm_networkcidr_display', isShow);
+});
+
+var vmHostName = $extension.find('#vm_hostname');
+vmHostName.on('change', function () {
+    removedClass(vmHostName, "empty");
+    vmName = vmHostName.data('item').name;
+});
+
+var vmNetworkCidr = $extension.find('#vm_networkcidr');
+vmNetworkCidr.on('change', function () {
+    removedClass(vmNetworkCidr, "empty");
+    vmNetworkCidrLabel = vmNetworkCidr.data('item').label;
+});
+
+$('#input_json').on('change', function () {
+    inputJson = $('#input_json').val();
+    inputJson = inputJson.replace(/\n+$/m, '');
+    inputJson = inputJson && inputJson != '' ? JSON.parse(inputJson) : null;
+});
+
+
+$('#add_vm').click(function () {
+    if (!$(this).hasClass("disabled")) {
+
+        if (validateFields()) {
+            alert('Заполните корректно обязательные поля');
+            return;
+        };
+        vmCounter++;
+        $('#delete_vm').show();
+        removedClass($("#delete_vm"), "disabled");
+
+        updateJson(inputJson);
+        addInputFormRecord(inputJson);
+        clearNetwork();
+
+        if (vmCounter >= maxVM) {
+            $(this).addClass("disabled");
+            $(this).hide();
+            return;
+        }
+    };
+});
+
+
+$("#delete_vm").on("click", function () {
+    if (!$(this).hasClass("disabled")) {
+        $("#add_vm").show();
+        $("#add_vm").removeClass("disabled");
+
+        deleteJson(inputJson);
+        addInputFormRecord(inputJson);
+
+        vmCounter--;
+        if (vmCounter == 0) {
+            $(this).addClass("disabled");
+            $(this).hide();
+        }
+
+    };
+});
+
+$("#finish").on("change", function () {
+    if (createVM || createVM == false) {
+      if ($(this).is(":checked")) {
+        if (inputJson && inputJson != null &&
+          inputJson != "" && $("#name_is").val()) {
+          finishChecked();
+        } else {
+          alert("Не заполнены обязательные поля. Поставьте галочку для: Заполнение формы завершено или заполните данные для Виртуальной машины");
+          $(this).prop("checked", false);
+          if (!$("#name_is").val()) {
+            $("#name_is").addClass("empty");
+          };
+          $("#finish").addClass("required");
+        };
+  
+      } else {
+        finishUnChecked();
+      }
+    } else {
+      finishChecked();
+    }
+  });
+
+
+function validateFields() {
+   
+    var isError = false;
+
+    if (!$('#name_is').val()) {
+        $('#name_is').addClass('empty');
+        isError = true;
+        return isError;
+    };
+    if ($('#vm_ni_operation').val() == "add") {
+        if (!$('#vm_hostname').val()) {
+            $('#vm_hostname').addClass('empty');
+            isError = true;
+            return isError;
+        };
+        if (!$('#vm_networkcidr_action').val()) {
+            $('#vm_networkcidr_action').addClass('empty');
+            isError = true;
+            return isError;
+        };
+        if ($('#vm_networkcidr_action').val() == "существующая") {
+            if (!$('#vm_networkcidr').val()) {
+                $('#vm_networkcidr').addClass('empty');
+                isError = true;
+                return isError;
+            };
+        };
+    };
+    return isError;
+
+};
+
+function visiabileRemoveNetwork(isShow) {
+    for (var i = 1; i < 11; i++) {
+        toggleRowVisibility('#network' + i, isShow);
+        if (!isShow) {
+            clearFields('#vm_hostname' + i, false);
+            clearFields('#vm_ni' + i, false);
+        }
+    };
+};
+
+function visiabileAddNetwork(isShow) {
+    toggleRowVisibility('#addNetwork', isShow);
+    $('#vm_hostname_required').toggleClass('required', isShow);
+    $('#vm_networkcidr_action').toggleClass('required', isShow);
+
+    if (!isShow) {
+        clearAddNetwork();
+    };
+};
+
+function clearNetwork() {
+    clearAddNetwork();
+};
+
+function clearAddNetwork() {
+    $('#vm_networkcidr_required').toggleClass('required', false);
+    toggleRowVisibility('#vm_networkcidr_display', false);
+    clearFields('#vm_hostname', false);
+    clearFields('#vm_networkcidr_action', false);
+    clearFields('#vm_networkcidr', false);
+    vmNetworkCidrLabel = "";
+};
+
+function updateJson(input_json) {
+    var newArray = input_json ? input_json.nodes : [];
+    var newVM = {
+        hostname: vmName,
+        vm_id: vmHostName.val(),
+        vm_networkcidr: vmNetworkCidrLabel
+    };
+    newArray.push(newVM);
+    var nodes = {
+        "nodes": newArray
+    };
+    $("#input_json").val(JSON.stringify(nodes)).change();
+};
+
+function deleteJson(input_json) {
+    var newArray = input_json ? input_json.nodes : [];
+    if (newArray.length > 0) {
+        var lastVM = newArray.pop();
+        if (newArray.length == 0) {
+            $("#input_json").val(null);
+        } else {
+            var nodes = { "nodes": newArray };
+            $("#input_json").val(JSON.stringify(nodes)).change();
+        }
+    }
+}
+
+function addInputFormRecord(input_json) {
+    var VMsData = input_json.nodes;
+    if (VMsData.length == 0) {
+        $('#input_form').val(null);
+    };
+    var table = ' <table id="vm_table" border="1"><tr><th>';
+    table += '№';
+    table += '</th><th>';
+    table += 'hostname';
+    table += '</th><th>';
+    table += 'networkCidr';
+    table += '</th>';
+
+    for (var index = 0; index < VMsData.length; index++) {
+        table += '<tr><td>';
+        table += index + 1;
+        table += '</td><td>';
+        table += (VMsData[index].hostname || '');
+        table += '</td><td>';
+        table += (VMsData[index].vm_networkcidr || '');
+        table += '</td>';
+    };
+    table += '</table>';
+    $('#input_form').val({ html: table });
+};
+
+function finishChecked() {
+    $("#netwotk_display").hide();
+    $("#name_is").readonly(true);
+    $("#vm_ni_operation").readonly(true);
+    clearAddNetwork();
+};
+
+function finishUnChecked() {
+    $("#netwotk_display").show();
+    if (vmCounter < maxVM) {
+        $("#add_vm").show();
+    }
+    if (vmCounter > 1) {
+        $("#delete_vm").show();
+    }
+    $("#name_is").readonly(false);
+    $("#vm_ni_operation").readonly(false);
+};
 
 $("#is_reopen").on("change", function () {
     if ($(this).is(":checked")) {
@@ -91,3 +345,14 @@ $("#is_reopen").on("change", function () {
         $("#correctness_block").hide();
     }
 });
+
+$(document).ready(function () {
+    if ($("#input_json").val()) {
+        //alert("createVM - false");    
+        createVM = false;
+    } else {
+        //alert("createVM - true");
+        createVM = true;
+    };
+});
+
